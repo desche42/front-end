03190138dc29717eb9ab11bb4bd448a6e1562bdd
desche42 <alsapi2004@gmail.com>
2018-02-13 15:03:13 +0100

    modificado gitignore y tasks para no subir ts compilados

---
+++ ws/ws_es6_ts/T10_variables_con_tipo.ts
@@ -0,0 +1,11 @@
+//declaro una variable tipo number
+let n:number ;
+n = 5;
+
+// esto dará un error de compilación (ver error del entorno de trabajo)
+// n = "hola";
+
+// declaro una función que toma dos números, y retorna un número
+function sumar(a:number,b:number):number{
+    return a-b 
+}
